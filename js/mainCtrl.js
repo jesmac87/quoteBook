cf3b400d3c764078c4d153d06dcebea07f42ffe7
Jesse Mace <jesmac87@gmail.com>
2015-02-14 12:38:42 -0700

    finish quoteBook

---
+++ js/mainCtrl.js
@@ -0,0 +1,34 @@
+var app = angular.module('quoteBook');
+
+app.controller('mainController', function($scope, dataService, $cookieStore) {
+
+    $scope.addName = '';
+    $scope.addQuote = '';
+    $scope.removeQuote = '';
+    $scope.sortBy = 'author';
+    $scope.order = true;
+
+    var getDataFromService = function() {
+        $scope.data = dataService.getData();
+    }();
+
+    $scope.removeQuoteObj = function() {
+        dataService.removeData($scope.removeQuote);
+        $scope.removeQuote = '';
+    };
+
+
+    $scope.addQuoteObj = function() {
+        dataService.addData({
+            text: $scope.addQuote,
+            author: $scope.addName,
+        });
+        $scope.addName = '';
+        $scope.addQuote = '';
+    };
+
+
+    //dataService.removeData('Life isn\'t about getting and having, it\'s about giving and being.');
+
+
+});
